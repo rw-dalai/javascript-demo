3ed3958fedca18220c6bf5e3f3dd86b591d2fad8
wenz <wenz@spengergasse.at>
2026-09-25 16:23:39 +0200

    feat: outline calculator TODOs

---
+++ calculator/main.js
@@ -0,0 +1,14 @@
+import { input } from '@inquirer/prompts';
+
+// TODO Read number 1 as long as its not a number
+
+const inputNumber1 = await input({ message: 'Enter number 1' });
+console.log(inputNumber1);
+
+// TODO Read number 2
+
+// TODO Read operator
+
+// TODO Calculate result
+
+// TODO output result
